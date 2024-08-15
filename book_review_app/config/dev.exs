f9ddd02e1d5467aff5360a1abfe56e3529a2cb09
Benjamin Figueroa Guzman <bfigueroa@miuandes.cl>
2024-08-15 13:35:58 -0400

    chore: Update dev.exs and docker-compose.yml files

--- book_review_app/config/dev.exs
+++ book_review_app/config/dev.exs
@@ -7,6 +7,7 @@ config :book_review_app, BookReviewApp.Repo,
   hostname: "localhost",
   database: "book_review_app_dev",
   stacktrace: true,
+  url: System.get_env("DATABASE_URL"),
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
@@ -19,7 +20,7 @@ config :book_review_app, BookReviewApp.Repo,
 config :book_review_app, BookReviewAppWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
